e7887963f3c86f6a08c63d058c6c2885919654c3
saitunc <saitunc47@gmail.com>
2025-05-03 21:33:56 +0300

    test: add unhappy test cases for signature verification with wrong public key and signature

--- src/signatureVerifier.test.ts
+++ src/signatureVerifier.test.ts
@@ -4,6 +4,7 @@ import { Bigint2048 } from './rsa.js';
 
 import { BLOCK_SIZES } from './utils.js';
 import { getQRData } from './getQRData.js';
+import { sign } from 'o1js/dist/node/mina-signer/src/signature.js';
 
 const proofsEnabled = false;
 
@@ -146,9 +147,58 @@ describe('Signature Verifier', () => {
     });
 
     it('should reject verification with tampered signature', async () => {
-      
 
+      const wrongSignature = signatureBigint.modSquare(publicKeyBigint);
+        
+      // Now split at your desired boundaries (multiple of 64 bytes)
+      const pad1 = Bytes.from(
+        paddedData.toBytes().slice(0, BLOCK_SIZES.MEDIUM)
+      );
 
+      const pad2 = Bytes.from(
+        paddedData.toBytes().slice(BLOCK_SIZES.MEDIUM, BLOCK_SIZES.LARGE)
+      );
+
+      const pad3 = Bytes.from(paddedData.toBytes().slice(BLOCK_SIZES.LARGE));
+
+      const proof1 = await SignatureVerifier.baseCase512(pad1, initialValue);
+      const proof2 = await SignatureVerifier.hashStep512(proof1.proof, pad2);
+      const proof3 = await SignatureVerifier.hashStep128(proof2.proof, pad3);
+      await expect(async () => {
+        await SignatureVerifier.verifySignature(
+          proof3.proof,
+          wrongSignature,
+          publicKeyBigint
+        )
+      }).rejects.toThrow('Field.assertEquals(): 9188579551671412591472664553230141 != 40899547542153145199703733613250199');
+    });
+    it.only('should reject verification with wrong public key', async () => {
+
+      // A completely different & weird value
+      const wrongPublicKeyHex = "94e81f7506595cc262addcfddd35d704055b2adf46dc619c56b48eee199995eca1a3254710620ac7801e976f44e3be454db0f190e3f7d4e3598972117344de52fcf7826f849488a959a7b3d21eb6dd03451662ea883eeeefde889a1499b9a47f9504c5f096c262b96d23d19750332d9e97eb6141d261de97994d4c4163ca9cbe3e077221b44253dcf81609428b68351ee3e9b60d2b351fdaa6ee8c28a845239f97de7cc0fe5d144e474813fb43ec583f81b4ee328c22167334898d210ba017a26ec68940f05df22bd9cc86bbc3a4354392372d566167769b735ba12ca3580f919c1bd8ba70c4c2ab0acf2b09bc2fae981f3c0295a6e1e9f248f50073094ffaf1";
+      const wrongPublicKey = Bigint2048.from(BigInt('0x' + wrongPublicKeyHex));
+        
+      // Now split at your desired boundaries (multiple of 64 bytes)
+      const pad1 = Bytes.from(
+        paddedData.toBytes().slice(0, BLOCK_SIZES.MEDIUM)
+      );
+
+      const pad2 = Bytes.from(
+        paddedData.toBytes().slice(BLOCK_SIZES.MEDIUM, BLOCK_SIZES.LARGE)
+      );
+
+      const pad3 = Bytes.from(paddedData.toBytes().slice(BLOCK_SIZES.LARGE));
+
+      const proof1 = await SignatureVerifier.baseCase512(pad1, initialValue);
+      const proof2 = await SignatureVerifier.hashStep512(proof1.proof, pad2);
+      const proof3 = await SignatureVerifier.hashStep128(proof2.proof, pad3);
+      await expect(async () => {
+        await SignatureVerifier.verifySignature(
+          proof3.proof,
+          signatureBigint,
+          wrongPublicKey
+        )
+      }).rejects.toThrow();
     });
   });
 });
