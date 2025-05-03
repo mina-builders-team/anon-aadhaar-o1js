d947ddcbaaf778141622d38d558186538670f999
saitunc <saitunc47@gmail.com>
2025-05-03 14:08:16 +0300

    test: add test case for age extractor

--- src/extractor.test.ts
+++ src/extractor.test.ts
@@ -1,5 +1,5 @@
 import { Field, Provable } from 'o1js';
-import { PHOTO_POSITION } from './constants.js';
+import { DOB_POSITION, PHOTO_POSITION } from './constants.js';
 import { paddedData } from './data-utils.js';
 import { DataExtractor, DelimiterExtractor } from './extractor.js';
 import {
@@ -17,7 +17,7 @@ describe('Extractor circuit tests', () => {
 
   beforeAll(async () => {
     await DelimiterExtractor.compile({ proofsEnabled });
-    await DataExtractor.compile({proofsEnabled});
+    await DataExtractor.compile({ proofsEnabled });
 
     const qrDataPadded = paddedData.toBytes();
 
@@ -28,7 +28,7 @@ describe('Extractor circuit tests', () => {
     nDelimitedData = createDelimitedData(qrData, photoIndex);
   }),
     describe('Delimiter circuit tests', () => {
-      it('should extract delimited data correctly', async () => {
+      it.skip('should extract delimited data correctly', async () => {
         const photoIndex = delimiterIndices[PHOTO_POSITION - 1].add(1);
 
         const proof1 = await DelimiterExtractor.extractData(qrData, photoIndex);
@@ -40,10 +40,10 @@ describe('Extractor circuit tests', () => {
         const summary = await DelimiterExtractor.analyzeMethods();
         console.log(summary);
       });
-  });
+    });
   describe('Extractor Circuit tests', () => {
     describe('Timestamp Extractor tests', () => {
-      it('Should extractor timestamp correcto', async () => {
+      it('should extract timestamp correct', async () => {
         const { proof } = await DataExtractor.timestamp(nDelimitedData);
         const output = proof.publicOutput;
 
@@ -53,6 +53,23 @@ describe('Extractor circuit tests', () => {
 
         expect(date).toEqual(expectedDate);
       });
+      it('should extract age correct', async () => {
+        const day = Field.from(1);
+        const month = Field.from(1);
+        const year = Field.from(2024);
+        const delimiterIndex = delimiterIndices[DOB_POSITION - 1];
+
+        const { proof } = await DataExtractor.age(
+          nDelimitedData,
+          delimiterIndex,
+          year,
+          month,
+          day
+        );
+        const output = proof.publicOutput;
+
+        expect(output.toBigInt()).toEqual(BigInt(40));
+      });
     });
-  })
+  });
 });
