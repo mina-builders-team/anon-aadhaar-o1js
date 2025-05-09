4109d2f33266ff333fd0fb45c2fcc36d5953b625
saitunc <saitunc47@gmail.com>
2025-05-10 01:10:05 +0300

    test: add 512-512-128 hash test to new file

--- src/recursiveHash.test.ts
+++ src/recursiveHash.test.ts
@@ -1,7 +1,8 @@
 import { Bytes, Gadgets, UInt32 } from 'o1js';
 import { RecursiveHash } from './recursiveHash.js';
 import { getQRData, TEST_DATA } from './getQRData.js';
-import { compute512BasedHashDigest } from './testUtils.js';
+import { compute512BasedHashDigest, computeChained128HashDigest } from './testUtils.js';
+import { BLOCK_SIZES } from './utils.js';
 
 let proofsEnabled = false;
 
@@ -29,11 +30,21 @@ describe('Recursive Hash tests', () => {
       async () => {
         // Now split at your desired boundaries (multiple of 64 bytes)
 
-        const finalDigest = await compute512BasedHashDigest(paddedData,initialValue);
+        const finalDigest = await computeChained128HashDigest(
+          paddedData,
+          initialValue
+        );
 
         const expectedDigest = Gadgets.SHA2.hash(256, signedData);
         expect(finalDigest.toHex()).toEqual(expectedDigest.toHex());
       }
     );
+
+    it('should compute partial hashing with byte blocks split by 512-512-128.', async () => {
+      const finalDigest = await compute512BasedHashDigest(paddedData,initialValue);
+
+      const expectedDigest = Gadgets.SHA2.hash(256, signedData);
+      expect(finalDigest.toHex()).toEqual(expectedDigest.toHex());
+    });
   });
 });
