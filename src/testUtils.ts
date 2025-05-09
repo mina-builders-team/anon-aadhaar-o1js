85d1b312007c2669613b0a175071bc88a8ff0116
saitunc <saitunc47@gmail.com>
2025-05-10 01:03:06 +0300

    feat: add hash utility functions that returns hash digest

--- src/testUtils.ts
+++ src/testUtils.ts
@@ -1,7 +1,7 @@
 import { Bytes, UInt32 } from 'o1js';
 import { BLOCK_SIZES } from './utils.js';
 import { RecursiveHash, RecursiveHashProof } from './recursiveHash.js';
-export { computed512BasedHash, computeChained128Hash };
+export { computed512BasedHash, computeChained128Hash,compute512BasedHashDigest };
 
 async function computed512BasedHash(
   paddedData: Bytes,
@@ -21,6 +21,33 @@ async function computed512BasedHash(
   const finalProof = await RecursiveHash.hashStep128(proof2.proof, pad3);
 
   return finalProof.proof;
+}
+
+async function compute512BasedHashDigest(
+  paddedData: Bytes,
+  initialValue: UInt32[]
+): Promise<Bytes> {
+  const byteData = paddedData.toBytes();
+
+  const pad1 = Bytes.from(byteData.slice(0, BLOCK_SIZES.MEDIUM));
+
+  const pad2 = Bytes.from(
+    byteData.slice(BLOCK_SIZES.MEDIUM, BLOCK_SIZES.LARGE)
+  );
+  const pad3 = Bytes.from(byteData.slice(BLOCK_SIZES.LARGE));
+
+  const proof1 = await RecursiveHash.baseCase512(pad1, initialValue);
+  const proof2 = await RecursiveHash.hashStep512(proof1.proof, pad2);
+  const finalProof = await RecursiveHash.hashStep128(proof2.proof, pad3);
+
+  const result = finalProof.proof.publicOutput;
+
+  // Get final digest
+  const finalDigest = Bytes.from(
+    result.hashState.flatMap((w: UInt32) => w.toBytesBE())
+  );
+
+  return finalDigest;
 }
 
 async function computeChained128Hash(
@@ -74,4 +101,64 @@ async function computeChained128Hash(
   );
 
   return finalProof.proof;
+}
+
+async function computeChained128HashDigest(
+  paddedData: Bytes,
+  initialValue: UInt32[]
+): Promise<Bytes> {
+  const paddedDataChunks = [];
+  for (let i = 0; i < 9; i++) {
+    paddedDataChunks[i] = Bytes.from(
+      paddedData
+        .toBytes()
+        .slice(i * BLOCK_SIZES.SMALL, BLOCK_SIZES.SMALL * (i + 1))
+    );
+  }
+  // Compute proofs recursively
+  const proof1 = await RecursiveHash.baseCase128(
+    paddedDataChunks[0],
+    initialValue
+  );
+  const proof2 = await RecursiveHash.hashStep128(
+    proof1.proof,
+    paddedDataChunks[1]
+  );
+  const proof3 = await RecursiveHash.hashStep128(
+    proof2.proof,
+    paddedDataChunks[2]
+  );
+  const proof4 = await RecursiveHash.hashStep128(
+    proof3.proof,
+    paddedDataChunks[3]
+  );
+  const proof5 = await RecursiveHash.hashStep128(
+    proof4.proof,
+    paddedDataChunks[4]
+  );
+  const proof6 = await RecursiveHash.hashStep128(
+    proof5.proof,
+    paddedDataChunks[5]
+  );
+  const proof7 = await RecursiveHash.hashStep128(
+    proof6.proof,
+    paddedDataChunks[6]
+  );
+  const proof8 = await RecursiveHash.hashStep128(
+    proof7.proof,
+    paddedDataChunks[7]
+  );
+  const finalProof = await RecursiveHash.hashStep128(
+    proof8.proof,
+    paddedDataChunks[8]
+  );
+
+  const result = finalProof.proof.publicOutput;
+
+  // Get final digest
+  const finalDigest = Bytes.from(
+    result.hashState.flatMap((w: UInt32) => w.toBytesBE())
+  );
+
+  return finalDigest;
 }
