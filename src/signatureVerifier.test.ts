94a9ae0c5bea3297085aa0aae9c2cef4ad48a96c
saitunc <saitunc47@gmail.com>
2025-05-10 00:38:27 +0300

    refactor: remove hash tests from signature verifier tests

--- src/signatureVerifier.test.ts
+++ src/signatureVerifier.test.ts
@@ -1,12 +1,11 @@
 import { SignatureVerifier } from './signatureVerifier.js';
 
-import { Bytes, Gadgets, UInt32, UInt8, 
-       } from 'o1js';
+import { Bytes, Gadgets, UInt32, UInt8 } from 'o1js';
 import { Bigint2048, rsaVerify65537 } from './rsa.js';
 
 import { BLOCK_SIZES, pkcs1v15Pad, pkcs1v15PadWrong } from './utils.js';
 import { getQRData, TEST_DATA, TEST_DATA_2 } from './getQRData.js';
-
+import { computeChained128Hash } from './testUtils.js';
 
 const proofsEnabled = false;
 
@@ -28,125 +27,6 @@ describe('Signature Verifier', () => {
     paddedData = inputs.paddedData;
     initialValue = inputs.initialValue;
     signedData = inputs.signedData;
-  });
-
-  describe('Partial hashing computations', () => {
-    // Warning: This step tests 128-byte hashing with 9 chunks, which is computationally heavier than other tests, so it takes more time to complete.
-    // It is executed only when proofsEnabled = true.
-    (!proofsEnabled ? it.skip : it)(
-      'should compute partial hashing with 9 byte blocks of size 128 bytes.',
-      async () => {
-        // Now split at your desired boundaries (multiple of 64 bytes)
-        const paddedDataChunks = [];
-        for (let i = 0; i < 9; i++) {
-          paddedDataChunks[i] = Bytes.from(
-            paddedData
-              .toBytes()
-              .slice(i * BLOCK_SIZES.SMALL, BLOCK_SIZES.SMALL * (i + 1))
-          );
-        }
-        // Compute proofs recursively
-        const proof1 = await SignatureVerifier.baseCase128(
-          paddedDataChunks[0],
-          initialValue
-        );
-        const proof2 = await SignatureVerifier.hashStep128(
-          proof1.proof,
-          paddedDataChunks[1]
-        );
-        const proof3 = await SignatureVerifier.hashStep128(
-          proof2.proof,
-          paddedDataChunks[2]
-        );
-        const proof4 = await SignatureVerifier.hashStep128(
-          proof3.proof,
-          paddedDataChunks[3]
-        );
-        const proof5 = await SignatureVerifier.hashStep128(
-          proof4.proof,
-          paddedDataChunks[4]
-        );
-        const proof6 = await SignatureVerifier.hashStep128(
-          proof5.proof,
-          paddedDataChunks[5]
-        );
-        const proof7 = await SignatureVerifier.hashStep128(
-          proof6.proof,
-          paddedDataChunks[6]
-        );
-        const proof8 = await SignatureVerifier.hashStep128(
-          proof7.proof,
-          paddedDataChunks[7]
-        );
-        const proof9 = await SignatureVerifier.hashStep128(
-          proof8.proof,
-          paddedDataChunks[8]
-        );
-
-        const result = proof9.proof.publicOutput;
-
-        // Get final digest
-        const finalDigest = Bytes.from(
-          result.hashState.flatMap((w: UInt32) => w.toBytesBE())
-        );
-
-        const expectedDigest = Gadgets.SHA2.hash(256, signedData);
-        expect(finalDigest.toHex()).toEqual(expectedDigest.toHex());
-      }
-    );
-
-    it('should compute partial hashing with byte blocks split by 512-512-128.', async () => {
-      const pad1 = Bytes.from(
-        paddedData.toBytes().slice(0, BLOCK_SIZES.MEDIUM)
-      );
-      const pad2 = Bytes.from(
-        paddedData.toBytes().slice(BLOCK_SIZES.MEDIUM, BLOCK_SIZES.LARGE)
-      );
-      const pad3 = Bytes.from(paddedData.toBytes().slice(BLOCK_SIZES.LARGE));
-
-      const proof1 = await SignatureVerifier.baseCase512(pad1, initialValue);
-      const proof2 = await SignatureVerifier.hashStep512(proof1.proof, pad2);
-      const proof3 = await SignatureVerifier.hashStep128(proof2.proof, pad3);
-
-      const result3 = proof3.proof.publicOutput;
-
-      // Get final digest
-      const finalDigest = Bytes.from(
-        result3.hashState.flatMap((w: UInt32) => w.toBytesBE())
-      );
-
-      const expectedDigest = Gadgets.SHA2.hash(256, signedData);
-      expect(finalDigest.toHex()).toEqual(expectedDigest.toHex());
-    });
-
-    it('should output different digest with wrong initial values', async () => {
-      const wrongInitialValue: UInt32[] = Gadgets.SHA2.initialState(224);
-
-      const pad1 = Bytes.from(
-        paddedData.toBytes().slice(0, BLOCK_SIZES.MEDIUM)
-      );
-      const pad2 = Bytes.from(
-        paddedData.toBytes().slice(BLOCK_SIZES.MEDIUM, BLOCK_SIZES.LARGE)
-      );
-      const pad3 = Bytes.from(paddedData.toBytes().slice(BLOCK_SIZES.LARGE));
-
-      const proof1 = await SignatureVerifier.baseCase512(
-        pad1,
-        wrongInitialValue
-      );
-      const proof2 = await SignatureVerifier.hashStep512(proof1.proof, pad2);
-      const proof3 = await SignatureVerifier.hashStep128(proof2.proof, pad3);
-
-      const result3 = proof3.proof.publicOutput;
-
-      // Get final digest
-      const finalDigest = Bytes.from(
-        result3.hashState.flatMap((w: UInt32) => w.toBytesBE())
-      );
-
-      const expectedDigest = Gadgets.SHA2.hash(256, signedData);
-      expect(finalDigest.toHex()).not.toEqual(expectedDigest.toHex());
-    });
   });
 
   describe('Signature verification computations', () => {
