66cd6db72be555c0c7bae66b56776640bf589d8e
saitunc <saitunc47@gmail.com>
2025-05-10 01:42:47 +0300

    refactor: use utility function in examples/run.ts

--- src/examples/run.ts
+++ src/examples/run.ts
@@ -1,8 +1,8 @@
-import { Bytes, verify } from 'o1js';
+import { verify } from 'o1js';
 import { SignatureVerifier } from '../signatureVerifier.js';
-import { BLOCK_SIZES } from '../utils.js';
 
 import { getQRData, TEST_DATA } from '../getQRData.js';
+import { compute512BasedHash } from '../testUtils.js';
 
 const { paddedData, initialValue, signatureBigint, publicKeyBigint } =
   getQRData(TEST_DATA);
@@ -13,28 +13,17 @@ console.time('Compile');
 const { verificationKey } = await SignatureVerifier.compile({ proofsEnabled });
 console.timeEnd('Compile');
 
-// Now split at your desired boundaries (Should be a multiple of 64 bytes).
-const pad1 = Bytes.from(paddedData.toBytes().slice(0, BLOCK_SIZES.MEDIUM));
-
-const pad2 = Bytes.from(
-  paddedData.toBytes().slice(BLOCK_SIZES.MEDIUM, BLOCK_SIZES.LARGE)
-);
-
-const pad3 = Bytes.from(paddedData.toBytes().slice(BLOCK_SIZES.LARGE));
-
-// Generate proofs step by step
 console.time('Proof generations');
-const proof1 = await SignatureVerifier.baseCase512(pad1, initialValue);
-const proof2 = await SignatureVerifier.hashStep512(proof1.proof, pad2);
-const proof3 = await SignatureVerifier.hashStep128(proof2.proof, pad3);
 
+const finalProof = await compute512BasedHash(paddedData, initialValue);
+
+console.timeEnd('Proof generations');
 // Now you can verify the RSA65537 signature. Should throw an error if verification fails.
 const { proof } = await SignatureVerifier.verifySignature(
-  proof3.proof,
+  finalProof,
   signatureBigint,
   publicKeyBigint
 );
-console.timeEnd('Proof generations');
 
 console.time('Verification');
 await verify(proof, verificationKey);
