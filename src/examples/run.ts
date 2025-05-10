af8ce524452b82bd521d4097eb6522511194a26e
saitunc <saitunc47@gmail.com>
2025-05-10 09:54:43 +0300

    fix: add RecursiveHash circuit compilation

--- src/examples/run.ts
+++ src/examples/run.ts
@@ -3,15 +3,20 @@ import { SignatureVerifier } from '../signatureVerifier.js';
 
 import { getQRData, TEST_DATA } from '../getQRData.js';
 import { compute512BasedHash } from '../testUtils.js';
+import { RecursiveHash } from '../recursiveHash.js';
 
 const { paddedData, initialValue, signatureBigint, publicKeyBigint } =
   getQRData(TEST_DATA);
 
 let proofsEnabled = true;
 
-console.time('Compile');
+console.time('Compile Hash Circuit');
+await RecursiveHash.compile({ proofsEnabled });
+console.timeEnd('Compile Hash Circuit');
+
+console.time('Compile Verifier Circuit');
 const { verificationKey } = await SignatureVerifier.compile({ proofsEnabled });
-console.timeEnd('Compile');
+console.timeEnd('Compile Verifier Circuit');
 
 console.time('Proof generations');
 
@@ -19,11 +24,14 @@ const finalProof = await compute512BasedHash(paddedData, initialValue);
 
 console.timeEnd('Proof generations');
 // Now you can verify the RSA65537 signature. Should throw an error if verification fails.
+
+
 const { proof } = await SignatureVerifier.verifySignature(
   finalProof,
   signatureBigint,
   publicKeyBigint
 );
+
 
 console.time('Verification');
 await verify(proof, verificationKey);
