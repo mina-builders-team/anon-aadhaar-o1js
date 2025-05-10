cd2ddb900cc81c18f5bba10609b42709f6adbb4d
saitunc <saitunc47@gmail.com>
2025-05-10 04:13:19 +0300

    refactor: use provable functions instead of zkprogram to test logic

--- src/extractor.test.ts
+++ src/extractor.test.ts
@@ -1,31 +1,35 @@
 import { Field } from 'o1js';
 import {
   DOB_POSITION,
-  GENDER_POSITION,
   PHOTO_POSITION,
-  PINCODE_POSITION,
-  STATE_POSITION,
 } from './constants.js';
 import {
   DataExtractor,
   DelimiterExtractor,
-  DataExtractorProof,
 } from './extractor.js';
 import {
   getDelimiterIndices,
   createPaddedQRData,
   createDelimitedData,
   intToCharString,
+  charBytesToInt,
 } from './utils.js';
 import { getQRData, TEST_DATA } from './getQRData.js';
+import {
+  ageAndGenderExtractor,
+  extractData,
+  pincodeExtractor,
+  stateExtractor,
+  timestampExtractor,
+} from './extractors.js';
 
 const proofsEnabled = false;
 
 describe('Extractor circuit tests', () => {
   let nDelimitedData: Field[];
-  let qrData: Field[];
+  let qrData: number[];
   let delimiterIndices: Field[];
-  let lastProof: DataExtractorProof;
+  let photoIndex: Field;
 
   beforeAll(async () => {
     await DelimiterExtractor.compile({ proofsEnabled });
@@ -35,102 +39,61 @@ describe('Extractor circuit tests', () => {
     const qrDataPadded = inputs.paddedData.toBytes();
 
     delimiterIndices = getDelimiterIndices(qrDataPadded).map(Field);
-    qrData = createPaddedQRData(inputs.paddedData.toBytes());
 
-    const photoIndex = delimiterIndices[PHOTO_POSITION - 1].add(1);
-    nDelimitedData = createDelimitedData(qrData, photoIndex);
+    qrData = createPaddedQRData(qrDataPadded);
 
-    lastProof = (await DataExtractor.createBaseProof()).proof;
+    photoIndex = delimiterIndices[PHOTO_POSITION - 1].add(1);
+    // Map to field for using it through tests.
+    nDelimitedData = createDelimitedData(qrData, Number(photoIndex)).map(Field);
   }),
     describe('Delimiter circuit tests', () => {
       it('should extract delimited data correctly', async () => {
-        const photoIndex = delimiterIndices[PHOTO_POSITION - 1].add(1);
+        const qrDataField = qrData.map(Field);
 
-        const proof1 = await DelimiterExtractor.extractData(qrData, photoIndex);
-        const delimitedData = proof1.proof.publicOutput;
+        const delimitedDataz = extractData(qrDataField, photoIndex);
 
-        expect(delimitedData).toEqual(nDelimitedData);
-
-        const summary = await DelimiterExtractor.analyzeMethods();
-        console.log(summary);
+        expect(delimitedDataz).toEqual(nDelimitedData);
       });
     });
   describe('Extractor Circuit tests', () => {
-    it('should extract timestamp correct', async () => {
-      const { proof } = await DataExtractor.timestamp(
-        lastProof,
-        nDelimitedData
-      );
-      const output = proof.publicOutput.TimeStamp;
+    it('should extract timestamp correctly', async () => {
+      const timestamp = timestampExtractor(nDelimitedData);
 
-      const date = new Date(Number(output) * 1000).getTime();
+      const date = new Date(Number(timestamp) * 1000).getTime();
 
       const expectedDate = new Date('2024-04-19T19:30:00.000Z').getTime();
 
       expect(date).toEqual(expectedDate);
     });
-    it('should extract age correct', async () => {
+    it('should extract age and gender correctly', async () => {
       const day = Field.from(1);
       const month = Field.from(1);
       const year = Field.from(2024);
       const delimiterIndex = delimiterIndices[DOB_POSITION - 1];
 
-      const { proof } = await DataExtractor.age(
-        lastProof,
+      const [age, gender] = ageAndGenderExtractor(
         nDelimitedData,
         delimiterIndex,
         year,
         month,
         day
       );
-      const outputAge = proof.publicOutput.Age;
-      const outputAgeAbove18 = proof.publicOutput.AgeAbove18;
-      expect(outputAge.toBigInt()).toEqual(40n);
-      expect(outputAgeAbove18.toBigInt()).toEqual(1n);
-    });
-    it('should find gender', async () => {
-      const genderIndex = delimiterIndices[GENDER_POSITION - 1].add(1);
 
-      const { proof } = await DataExtractor.gender(
-        lastProof,
-        nDelimitedData,
-        genderIndex
-      );  
-
-      const outputGender = proof.publicOutput.Gender;
-
-      expect(String.fromCharCode(Number(outputGender))).toEqual('M');
+      expect(age.toBigInt()).toEqual(40n);
+      expect(String.fromCharCode(Number(gender))).toEqual('M');
     });
     it('should get pincode ', async () => {
-      const pincodeIndex = delimiterIndices[PINCODE_POSITION - 1].add(1);
+      const pincode = pincodeExtractor(nDelimitedData, delimiterIndices);
 
-      const { proof } = await DataExtractor.pincode(
-        lastProof,
-        nDelimitedData,
-        pincodeIndex
-      );
-
-      const outputPincode = proof.publicOutput.Pincode;
-
-      expect(outputPincode.toBigInt()).toEqual(110051n);
+      expect(pincode.toBigInt()).toEqual(110051n);
     });
     it('should extract state', async () => {
-      const stateLength = delimiterIndices[STATE_POSITION].sub(
-        delimiterIndices[STATE_POSITION - 1]
-      );
+      const state = stateExtractor(nDelimitedData, delimiterIndices);
 
-      const stateIndex = delimiterIndices[STATE_POSITION - 1].add(1);
+      // Our state here is at length 5, so try:
+      const stateValue = charBytesToInt(state.slice(0, 5), 5);
 
-      const { proof } = await DataExtractor.state(
-        lastProof,
-        nDelimitedData,
-        stateIndex,
-      );
-
-      // Get the first array, which is the selected subarray
-      const outputState = proof.publicOutput.State[0];
-
-      expect(intToCharString(outputState, 5)).toEqual('Delhi');
+      expect(intToCharString(stateValue, 5)).toEqual('Delhi');
     });
   });
 });
