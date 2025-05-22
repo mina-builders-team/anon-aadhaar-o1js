c3df06b4c75f73ab997527d513b517440e4d935c
saitunc <saitunc47@gmail.com>
2025-05-22 16:58:09 +0300

    refactor: update run.ts for compatibility with new SignatureVerifier changes

--- src/examples/run.ts
+++ src/examples/run.ts
@@ -1,50 +1,38 @@
-import { Bytes32 } from '../dataTypes.js';
 import { SignatureVerifier } from '../signatureVerifier.js';
 
 import { getQRData, TEST_DATA } from '../getQRData.js';
 import { prepareRecursiveHashData } from '../testUtils.js';
 import {
   hashProgram,
-  hashWrapper,
-  recursiveHashProgram,
 } from '../recursion.js';
+import { verify } from 'o1js';
 
 
 const { signatureBigint, publicKeyBigint, signedData } = getQRData(TEST_DATA);
 
-let proofsEnabled = false;
+let proofsEnabled = true;
 
 console.time('Compile Hash Circuit');
 await hashProgram.compile({ proofsEnabled });
-await recursiveHashProgram.compile({ proofsEnabled });
-await hashWrapper.compile({ proofsEnabled });
 console.timeEnd('Compile Hash Circuit');
 
 console.time('Compile Verifier Circuit');
 const { verificationKey } = await SignatureVerifier.compile({ proofsEnabled });
 console.timeEnd('Compile Verifier Circuit');
 
-console.time('Proof generations');
 const preparedData = prepareRecursiveHashData(signedData);
-const finalProof = await hashWrapper.run(preparedData);
-const finalHash = Bytes32.from(
-  finalProof.proof.publicOutput.array.flatMap((x) => x.toBytesBE())
-);
 
-console.log(finalHash.toHex());
-console.timeEnd('Proof generations');
 // // Now you can verify the RSA65537 signature. Should throw an error if verification fails.
-
 console.time('Signature verification');
 
 const { proof } = await SignatureVerifier.verifySignature(
-  finalProof.proof,
+  preparedData,
   signatureBigint,
   publicKeyBigint
 );
 console.timeEnd('Signature verification');
 
-// Commented out for now due to a low level error: Bin_prot.Common.Buffer_short
-// console.time('Verification');
-// await verify(proof, verificationKey);
-// console.timeEnd('Verification');
+// When proofsEnabled:false, it throws an error. Problem solved, Yay!
+console.time('Verification');
+await verify(proof, verificationKey);
+console.timeEnd('Verification');
