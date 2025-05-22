5f9439abf1d3579d66fea4315e77a1f2927935d4
saitunc <saitunc47@gmail.com>
2025-05-22 17:23:01 +0300

    refactor(test): update tests with with new utility functions

--- src/recursiveHash.test.ts
+++ src/recursiveHash.test.ts
@@ -1,13 +1,12 @@
-import { Bytes, Gadgets } from 'o1js';
+import { Gadgets } from 'o1js';
 import { getQRData, TEST_DATA } from './getQRData.js';
-import { prepareRecursiveHashData } from './testUtils.js';
+import { generateHashFromData } from './testUtils.js';
 import {
   hashProgram,
-  hashRecursive,
   recursiveHashProgram,
 } from './recursion.js';
 
-let proofsEnabled = false;
+let proofsEnabled = true;
 
 describe('Recursive Hash tests', () => {
   let signedData: Uint8Array;
@@ -21,39 +20,26 @@ describe('Recursive Hash tests', () => {
     signedData = inputs.signedData;
   });
 
-  describe('Partial hashing computations', () => {
+  describe('Recursive hashing computations', () => {
     it('should compute recursive hash with abstracted hashing', async () => {
-      const blocks = prepareRecursiveHashData(signedData);
-
-      const digest = await hashRecursive.run(blocks);
-
-      const finalDigest = Bytes.from(
-        digest.array.flatMap((x) => x.toBytesBE())
-      );
+      const finalDigest = await generateHashFromData(signedData);
 
       const expectedDigest = Gadgets.SHA2.hash(256, signedData);
       expect(finalDigest.toHex()).toEqual(expectedDigest.toHex());
     });
     it('should return correct hash for a single block', async () => {
       const singleBlockData = signedData.slice(0, 64); // or appropriate block size
-      const blocks = prepareRecursiveHashData(singleBlockData);
 
-      const digest = await hashRecursive.run(blocks);
-      const finalDigest = Bytes.from(
-        digest.array.flatMap((x) => x.toBytesBE())
-      );
+      const finalDigest = await generateHashFromData(singleBlockData);
+
       const expectedDigest = Gadgets.SHA2.hash(256, singleBlockData);
 
       expect(finalDigest.toHex()).toEqual(expectedDigest.toHex());
     });
     it('should correctly hash data not aligned to block size', async () => {
       const weirdLengthData = signedData.slice(0, 45); // Example: not divisible by 64
-      const blocks = prepareRecursiveHashData(weirdLengthData);
+      const finalDigest = await generateHashFromData(weirdLengthData);
 
-      const digest = await hashRecursive.run(blocks);
-      const finalDigest = Bytes.from(
-        digest.array.flatMap((x) => x.toBytesBE())
-      );
       const expectedDigest = Gadgets.SHA2.hash(256, weirdLengthData);
 
       expect(finalDigest.toHex()).toEqual(expectedDigest.toHex());
