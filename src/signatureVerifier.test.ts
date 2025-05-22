c4232277db4fea3dccb5b1c57be495721ce79940
saitunc <saitunc47@gmail.com>
2025-05-22 17:02:24 +0300

    test: refactor tests to use utility functions and adapt to updated circuit

--- src/signatureVerifier.test.ts
+++ src/signatureVerifier.test.ts
@@ -1,16 +1,21 @@
 import { SignatureVerifier } from './signatureVerifier.js';
 
 import { Gadgets } from 'o1js';
-import { Bigint2048, rsaVerify65537 } from './rsa.js';
+import { Bigint2048 } from './rsa.js';
 
 import { pkcs1v15Pad } from './utils.js';
 import { getQRData, TEST_DATA, TEST_DATA_2 } from './getQRData.js';
 
-import { pkcs1v15PadWrong, prepareRecursiveHashData } from './testUtils.js';
+import {
+  pkcs1v15PadWrong,
+  prepareRecursiveHashData,
+  expectSignatureError,
+  expectSignatureCircuitError,
+} from './testUtils.js';
 
-import { hashProgram, hashWrapper, recursiveHashProgram } from './recursion.js';
+import { hashProgram, recursiveHashProgram } from './recursion.js';
 
-const proofsEnabled = false;
+const proofsEnabled = true;
 
 describe('Signature Verifier', () => {
   let publicKeyBigint: Bigint2048;
@@ -20,7 +25,6 @@ describe('Signature Verifier', () => {
   beforeAll(async () => {
     await hashProgram.compile({ proofsEnabled });
     await recursiveHashProgram.compile({ proofsEnabled });
-    await hashWrapper.compile({ proofsEnabled });
     await SignatureVerifier.compile({ proofsEnabled });
 
     const inputs = getQRData(TEST_DATA);
@@ -34,11 +38,9 @@ describe('Signature Verifier', () => {
     it('should verify rsa signature correctly', async () => {
       const blocks = prepareRecursiveHashData(signedData);
 
-      const { proof } = await hashWrapper.run(blocks);
-
       // Should throw an error if verification fails.
       await SignatureVerifier.verifySignature(
-        proof,
+        blocks,
         signatureBigint,
         publicKeyBigint
       );
@@ -47,21 +49,23 @@ describe('Signature Verifier', () => {
     it('should reject verification with tampered signature', async () => {
       const wrongSignature = signatureBigint.modSquare(publicKeyBigint);
       const blocks = prepareRecursiveHashData(signedData);
+      // Error message to be used in proofsEnabled:true option
+      const expectedMsgTrue =
+        'Equal 9188579551671412591472664553230141 40899547542153145199703733613250199';
+      // Error message to be used in proofsEnabled:false option
+      const expectedMsgFalse =
+        'Field.assertEquals(): 9188579551671412591472664553230141 != 40899547542153145199703733613250199';
 
-      const { proof } = await hashWrapper.run(blocks);
-
-      const isVerified = async () => {
-        await SignatureVerifier.verifySignature(
-          proof,
-          wrongSignature,
-          publicKeyBigint
-        );
-      };
-
-      await expect(isVerified).rejects.toThrow(
-        'Field.assertEquals(): 9188579551671412591472664553230141 != 40899547542153145199703733613250199'
+      await expectSignatureCircuitError(
+        blocks,
+        wrongSignature,
+        publicKeyBigint,
+        expectedMsgTrue,
+        expectedMsgFalse,
+        proofsEnabled
       );
     });
+
     it('should reject verification with wrong public key', async () => {
       // A completely different & weird value
       const wrongPublicKeyHex =
@@ -70,18 +74,20 @@ describe('Signature Verifier', () => {
 
       const blocks = prepareRecursiveHashData(signedData);
 
-      const { proof } = await hashWrapper.run(blocks);
+      // Error message to be used in proofsEnabled:true option
+      const expectedMsgTrue =
+        'Equal 9188579551671412591472664553230141 19665786662882214150578387368928633';
+      // Error message to be used in proofsEnabled:false option
+      const expectedMsgFalse =
+        'Field.assertEquals(): 9188579551671412591472664553230141 != 19665786662882214150578387368928633';
 
-      const isVerified = async () => {
-        await SignatureVerifier.verifySignature(
-          proof,
-          signatureBigint,
-          wrongPublicKey
-        );
-      };
-
-      await expect(isVerified).rejects.toThrow(
-        'Field.assertEquals(): 9188579551671412591472664553230141 != 19665786662882214150578387368928633'
+      await expectSignatureCircuitError(
+        blocks,
+        signatureBigint,
+        wrongPublicKey,
+        expectedMsgTrue,
+        expectedMsgFalse,
+        proofsEnabled
       );
     });
 
@@ -95,15 +101,15 @@ describe('Signature Verifier', () => {
       // This test should fail especially for distorted padded bytes
       const blocks = prepareRecursiveHashData(randomizedData);
 
-      const { proof } = await hashWrapper.run(blocks);
-
       const isVerified = async () => {
         await SignatureVerifier.verifySignature(
-          proof,
+          blocks,
           signatureBigint,
           publicKeyBigint
         );
       };
+
+      // Random data throws different messages every time, so trying to get the expected message is an unnecessary effort.
       await expect(isVerified).rejects.toThrow();
     });
 
@@ -112,81 +118,88 @@ describe('Signature Verifier', () => {
 
       //Steps below are identical to the `verifySignature` method steps.
 
-      const paddedHash = pkcs1v15Pad(digest);
-      // First, try it with padded hash value.
+      // Error message to be used in proofsEnabled:true option
+      const expectedMsgTrue =
+        'Equal 15569117 32614225132466096153625214992617693';
+      // Error message to be used in proofsEnabled:false option
+      const expectedMsgFalse =
+        'Field.assertEquals(): 15569117 != 32614225132466096153625214992617693';
 
-      rsaVerify65537(paddedHash, signatureBigint, publicKeyBigint);
-
-      // Now with non-padded hash.
-      const nonPaddedHash = Bigint2048.from(BigInt('0x' + digest.toHex()));
-
-      const isVerified = async () => {
-        rsaVerify65537(nonPaddedHash, signatureBigint, publicKeyBigint);
-      };
-
-      await expect(isVerified).rejects.toThrow();
+      const hash = Bigint2048.from(BigInt('0x' + digest.toHex()));
+      // In out of circuit verifications, error is always printed in Field.assertEquals(). So even if proofsEnabled is true, we'll pass it as false to get the correct type of error message.
+      await expectSignatureError(
+        hash,
+        signatureBigint,
+        publicKeyBigint,
+        expectedMsgTrue,
+        expectedMsgFalse,
+        false
+      );
     });
 
     it('should reject verification when hash data is padded with incorrect PKCS#1 v1.5 format ', async () => {
       const digest = Gadgets.SHA2.hash(256, signedData);
 
-      //Steps below are identical to the `verifySignature` method steps.
+      // Error message to be used in proofsEnabled:true option
+      const expectedMsgTrue =
+        'Equal 32512813084447837801505478736187613 32614225132466096153625214992617693';
+      // Error message to be used in proofsEnabled:false option
+      const expectedMsgFalse =
+        'Field.assertEquals(): 32512813084447837801505478736187613 != 32614225132466096153625214992617693';
 
-      const wronglyPaddedHash = pkcs1v15PadWrong(digest);
-      // First, try it with padded hash value.
+      const paddedHash = pkcs1v15PadWrong(digest);
 
-      const isVerified = async () => {
-        rsaVerify65537(wronglyPaddedHash, signatureBigint, publicKeyBigint);
-      };
-
-      await expect(isVerified).rejects.toThrow();
-    });
-
-    it('should reject verification when hash data is padded with incorrect PKCS#1 v1.5 format ', async () => {
-      const digest = Gadgets.SHA2.hash(256, signedData);
-
-      //Steps below are identical to the `verifySignature` method steps.
-
-      const wronglyPaddedHash = pkcs1v15PadWrong(digest);
-      // First, try it with padded hash value.
-
-      const isVerified = async () => {
-        rsaVerify65537(wronglyPaddedHash, signatureBigint, publicKeyBigint);
-      };
-
-      await expect(isVerified).rejects.toThrow();
+      // In out of circuit verifications, error is always printed in Field.assertEquals(). So even if proofsEnabled is true, we'll pass it as false to get the correct type of error message.
+      await expectSignatureError(
+        paddedHash,
+        signatureBigint,
+        publicKeyBigint,
+        expectedMsgTrue,
+        expectedMsgFalse,
+        false
+      );
     });
 
     it('should reject signatures with SHA-384 digests ', async () => {
       const digest = Gadgets.SHA2.hash(384, signedData);
 
-      //Steps below are identical to the `verifySignature` method steps.
+      // Error message to be used in proofsEnabled:true option
+      const expectedMsgTrue =
+        'Equal 31644031178026440823572775767911610 9188579551671412591472664553230141';
+      // Error message to be used in proofsEnabled:false option
+      const expectedMsgFalse =
+        'Field.assertEquals(): 31644031178026440823572775767911610 != 9188579551671412591472664553230141';
 
       const paddedHash = pkcs1v15Pad(digest);
-      // First, try it with padded hash value.
-
-      const isVerified = async () => {
-        rsaVerify65537(paddedHash, signatureBigint, publicKeyBigint);
-      };
-
-      await expect(isVerified).rejects.toThrow(
-        'Field.assertEquals(): 31644031178026440823572775767911610 != 9188579551671412591472664553230141'
+      // In out of circuit verifications, error is always printed in Field.assertEquals(). So even if proofsEnabled is true, we'll pass it as false to get the correct type of error message.
+      await expectSignatureError(
+        paddedHash,
+        signatureBigint,
+        publicKeyBigint,
+        expectedMsgTrue,
+        expectedMsgFalse,
+        false
       );
     });
+
     it('should reject signatures with SHA-512 digests ', async () => {
       const digest = Gadgets.SHA2.hash(512, signedData);
 
-      //Steps below are identical to the `verifySignature` method steps.
+      const expectedMsgTrue =
+        'Equal 50469815090039084110515593114079551 9188579551671412591472664553230141';
+      // Error message to be used in proofsEnabled:false option
+      const expectedMsgFalse =
+        'Field.assertEquals(): 50469815090039084110515593114079551 != 9188579551671412591472664553230141';
 
       const paddedHash = pkcs1v15Pad(digest);
-      // First, try it with padded hash value.
-
-      const isVerified = async () => {
-        rsaVerify65537(paddedHash, signatureBigint, publicKeyBigint);
-      };
-
-      await expect(isVerified).rejects.toThrow(
-        'Field.assertEquals(): 50469815090039084110515593114079551 != 9188579551671412591472664553230141'
+      // In out of circuit verifications, error is always printed in Field.assertEquals(). So even if proofsEnabled is true, we'll pass it as false to get the correct type of error message.
+      await expectSignatureError(
+        paddedHash,
+        signatureBigint,
+        publicKeyBigint,
+        expectedMsgTrue,
+        expectedMsgFalse,
+        false
       );
     });
     it('should reject signature verification of a different data', async () => {
@@ -195,52 +208,63 @@ describe('Signature Verifier', () => {
 
       const blocks = prepareRecursiveHashData(otherData);
 
-      const { proof } = await hashWrapper.run(blocks);
+      const expectedMsgTrue =
+        'Equal 2914443462457906719845331827558144 9188579551671412591472664553230141';
+      // Error message to be used in proofsEnabled:false option
+      const expectedMsgFalse =
+        'Field.assertEquals(): 2914443462457906719845331827558144 != 9188579551671412591472664553230141';
 
-      const isVerified = async () => {
-        await SignatureVerifier.verifySignature(
-          proof,
-          signatureBigint,
-          publicKeyBigint
-        );
-      };
-
-      await expect(isVerified).rejects.toThrow();
+      await expectSignatureCircuitError(
+        blocks,
+        signatureBigint,
+        publicKeyBigint,
+        expectedMsgTrue,
+        expectedMsgFalse,
+        proofsEnabled
+      );
     });
+
     it('should reject signature verification of a different signature', async () => {
       const inputs = getQRData(TEST_DATA_2);
       const otherSignature = inputs.signatureBigint;
 
       const blocks = prepareRecursiveHashData(signedData);
 
-      const { proof } = await hashWrapper.run(blocks);
+      const expectedMsgTrue =
+        'Equal 9188579551671412591472664553230141 2914443462457906719845331827558144';
+      // Error message to be used in proofsEnabled:false option
+      const expectedMsgFalse =
+        'Field.assertEquals(): 9188579551671412591472664553230141 != 2914443462457906719845331827558144';
 
-      const isVerified = async () => {
-        await SignatureVerifier.verifySignature(
-          proof,
-          otherSignature,
-          publicKeyBigint
-        );
-      };
-
-      await expect(isVerified).rejects.toThrow();
+      await expectSignatureCircuitError(
+        blocks,
+        otherSignature,
+        publicKeyBigint,
+        expectedMsgTrue,
+        expectedMsgFalse,
+        proofsEnabled
+      );
     });
+
     it('should reject signature verification of empty data', async () => {
       const EMPTY_DATA = signedData.slice(0, 0);
 
       const blocks = prepareRecursiveHashData(EMPTY_DATA);
 
-      const { proof } = await hashWrapper.run(blocks);
+      const expectedMsgTrue =
+        'Equal 74028607801358283505814219305171029 9188579551671412591472664553230141';
+      // Error message to be used in proofsEnabled:false option
+      const expectedMsgFalse =
+        'Field.assertEquals(): 74028607801358283505814219305171029 != 9188579551671412591472664553230141';
 
-      const isVerified = async () => {
-        await SignatureVerifier.verifySignature(
-          proof,
-          signatureBigint,
-          publicKeyBigint
-        );
-      };
-
-      await expect(isVerified).rejects.toThrow();
+      await expectSignatureCircuitError(
+        blocks,
+        signatureBigint,
+        publicKeyBigint,
+        expectedMsgTrue,
+        expectedMsgFalse,
+        proofsEnabled
+      );
     });
   });
 });
