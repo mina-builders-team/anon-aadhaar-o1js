c482f96a3890d7cf6f5ea011412d45fbeff69170
saitunc <saitunc47@gmail.com>
2025-06-06 14:34:24 +0300

    refactor: rename variable name in nullifier

--- src/nullifier.ts
+++ src/nullifier.ts
@@ -2,6 +2,6 @@ import { Field, Poseidon } from 'o1js';
 export { nullifier };
 
 function nullifier(nullifierSeed: Field, photo: Field[]) {
-  const first16 = Poseidon.hash(photo);
-  return Poseidon.hash([nullifierSeed, first16]);
+  const photoHash = Poseidon.hash(photo);
+  return Poseidon.hash([nullifierSeed, photoHash]);
 }
