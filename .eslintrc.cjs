eb62d249b244949c3be8c08c52dc0559cd8f3b7d
Raunaque97 <rpatra1997@gmail.com>
2025-06-27 02:10:56 +0530

    moved eslint dependencies and .prettierrc  to root level.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,25 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
-    node: true,
-    jest: true,
+    es2021: true,
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:o1js/recommended',
+    'next',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'o1js'],
   rules: {
-    'no-constant-condition': 'off',
-    'prefer-const': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' },
+    ],
   },
 };
