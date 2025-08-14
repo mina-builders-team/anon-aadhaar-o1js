622eaf3c1dab33c28113dc3f5e29a28a47ce443a
Raunaque97 <rpatra1997@gmail.com>
2025-08-14 13:12:07 +0530

    fix: spec compilation issue with Operation.compute(...)

--- packages/core/src/presentationSpecs.ts
+++ packages/core/src/presentationSpecs.ts
@@ -1,18 +1,25 @@
 import { Spec, Claim, Operation, Node } from 'mina-attestations';
 import { AadhaarCredentialFactory } from './AadhaarCredential.js';
-import { UInt64, Field } from 'o1js';
+import { UInt64, Field, Provable } from 'o1js';
 
-export function calculateAge(currentYear: Field, currentMonth: Field, currentDay: Field, dobYear: Field, dobMonth: Field, dobDay: Field) {
-  const ageByYear = currentYear.sub(dobYear).sub(Field(1))
+export function calculateAge(
+  currentYear: Node<Field>,
+  currentMonth: Node<Field>,
+  currentDay: Node<Field>,
+  dobYear: Node<Field>,
+  dobMonth: Node<Field>,
+  dobDay: Node<Field>
+) {
+  const ageByYear =  Operation.sub(Operation.sub(currentYear, dobYear), Operation.constant(Field(1)))
 
   // Check if current month > DOB month or if same month and current day >= DOB day
-  const monthGt = currentMonth.greaterThan(dobMonth).toField()
-  const monthEq = currentMonth.equals(dobMonth).toField()
-  const dayGt = currentDay.add(Field(1)).greaterThan(dobDay).toField()
-  const isHigherDayOnSameMonth = monthEq.mul(dayGt)
-
+  const monthGt = Operation.lessThan(dobMonth, currentMonth)
+  const monthEq = Operation.equals(dobMonth, currentMonth)
+  const dayGt = Operation.lessThan(dobDay, Operation.add(currentDay, Operation.constant(Field(1))))
+  const isHigherDayOnSameMonth = Operation.and(monthEq, dayGt)
   // Final age calculation
-  const age = ageByYear.add(monthGt.add(isHigherDayOnSameMonth))
+  const age = Operation.add(ageByYear, 
+    Operation.ifThenElse(Operation.or(monthGt, isHigherDayOnSameMonth), Operation.constant(Field(1)), Operation.constant(Field(0))))
   return age
 }
 
@@ -28,13 +35,12 @@ export async function ageMoreThan18Spec() {
       const dobDay = Operation.property(credential, "DobDay");
       const dobMonth = Operation.property(credential, "DobMonth");
       const dobYear = Operation.property(credential, "DobYear");
-      const age = Operation.compute(
-        [currentYear, currentMonth, currentDay, dobYear, dobMonth, dobDay],
-        Field,
-        calculateAge
-      );
+      const age = calculateAge(currentYear, currentMonth, currentDay, dobYear, dobMonth, dobDay)
 
-      const assert = Operation.lessThanEq(Operation.constant(UInt64.from(18)), age)
+      Provable.asProver(() => {
+        console.log("Age: ", age.toString())
+      })
+      const assert = Operation.lessThanEq(Operation.constant(Field(18)), age)
 
       return { assert, outputClaim: Operation.property(credential, "pubKeyHash") };
     } 
